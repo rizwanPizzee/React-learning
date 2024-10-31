b3d49e51530d76a79c88929eab43f4a12beb4619
Pizz-ee <160001581+Pizz-ee@users.noreply.github.com>
2024-10-31 14:31:56 +0500

    learn and Practicing with props and propTypes.

--- my-react-app-new/src/App.jsx
+++ my-react-app-new/src/App.jsx
@@ -1,22 +1,12 @@
-import Header from './Header.jsx';
-import Footer from './Footer.jsx'
-import Food from './Food.jsx';
 import Card from './Card.jsx';
-
+import Student from './Student.jsx';
 function App() {
   return(
     <>
-      <Header/>
-      <Food/>
-      <Footer/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Student name="Ali" age={14} isStudent={true}/>
+      <Student name="Ahmed" age={20} isStudent={false}/>
+      <Student name="Raza" age={25} isStudent={true}/>
     </>
-    
   );
 }
 
