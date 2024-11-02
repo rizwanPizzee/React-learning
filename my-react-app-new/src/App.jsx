24ee0be10362be041d079813d75f8d75aeb188cb
Pizz-ee <160001581+Pizz-ee@users.noreply.github.com>
2024-11-02 23:55:15 +0500

    Practice with multiple list on single Component for Rendering List( also used the defaultProps and propTypes methods)

--- my-react-app-new/src/App.jsx
+++ my-react-app-new/src/App.jsx
@@ -1,9 +1,27 @@
 import List from './List.jsx';
 
 function App() {
+  const fruits = [{id: 1, name: "Apple", calories: 95},
+                  {id: 2, name: "Pineapple", calories: 37},
+                  {id: 3, name: "Banana", calories: 105},
+                  {id: 4, name: "Orange", calories: 45},
+                  {id: 5, name: "Coconut", calories:159}];
+
+  const vegetables = [{id: 1, name: "Tomato", calories: 126},
+                    {id: 2, name: "Potato", calories: 75},
+                    {id: 3, name: "Spinch", calories: 201},
+                    {id: 4, name: "Carrot", calories: 162},
+                    {id: 5, name: "Onion", calories: 54}];
+                  
   return(
     <>
-    <List/>
+    {fruits.length > 0 ? <List items = {fruits} category = "Fruits"/>: null}
+    {vegetables.length > 0 ? <List items = {vegetables} category = "Vegetables"/> : null}
+  
+  {/*
+    {fruits.length > 0 && <List items = {fruits} category = "Fruits"/>}
+    {vegetables.length > 0 && <List items = {vegetables} category = "Vegetables"/> }
+  */}
     </>
   );
 }
--- my-react-app-new/src/List.jsx
+++ my-react-app-new/src/List.jsx
@@ -1,39 +1,29 @@
+import PropTypes from 'prop-types';
+function List(props){
+    const category = props.category;
+    const listOfItems = props.items;
+    const itemsList = listOfItems.map((values) =>
+                <li key={values.id}>{values.name}: &nbsp; <b>{values.calories}</b></li>);
 
-function List(){
-    const fruits = [{id: 1, name: "Apple", calories: 95},
-                    {id: 2, name: "Pineapple", calories: 37},
-                    {id: 3, name: "Banana", calories: 105},
-                    {id: 4, name: "Orange", calories: 45},
-                    {id: 5, name: "Coconut", calories:159}];
-
-    // using of filter method
-    //const lowCalfruits = fruits.filter(values => values.calories < 100);
-    const highCalfruits = fruits.filter(values => values.calories > 100);
-    const fruit = highCalfruits.map((value) =>
-                <li key={value.id}>{value.name}: &nbsp; <b>{value.calories}</b></li>);
-
-    return( <ol>{fruit}</ol>);
-
-/*  //Sorting
-    //fruits.sort((a,b)=> a.name.localeCompare(b.name)); //Alphabetical Ascending
-    //fruits.sort((a,b)=> b.name.localeCompare(a.name)); //Alphabetical Descending
-    //fruits.sort((a,b)=> a.calories - b.calories); //Ascending Sort by num.
-    //fruits.sort((a,b)=> b.calories - a.calories);  //Descending Sort by num.
-
-    const fruit = fruits.map((value) =>
-                <li key={value.id}>{value.name}: &nbsp; <b>{value.calories}</b></li>);
-
-    return( <ol>{fruit}</ol>);
-
-*/
-
-/*  //simple list returning 
-    const fruits = ["Apple","Pineapple", "Banana", "Orange"];
-    fruits.sort();
-    const fruit = fruits.map((value) =><li>{value}</li>);
-    return( <ol>{fruit}</ol>);
-*/
+    return( <>
+                <h3 className="category-list">{category}</h3>
+                <ol className="itemsList-list">{itemsList}</ol>
+            </> 
+    );
 
 }
+List.defaultProps = {
+    items: [],
+    category: "Category"
+};
+
+List.propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        calories: PropTypes.number
+    })),
+    category: PropTypes.string
+};
 
 export default List;
